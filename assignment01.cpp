6606a2ce8eb582ceaf85a7a2d88c0f0777c8955e
Pavankumar07s <pawankumar14662693@gmail.com>
2024-08-14 11:33:34 +0530

    quickSort error done

--- assignment01.cpp
+++ assignment01.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 struct Student
@@ -21,102 +21,134 @@ void DisplayStruct(struct Student *s[], int size)
 {
     for (int i = 0; i < size; i++)
     {
-        cout << "student name :" << s[i]->name << endl;
-        cout << "Roll number :" << s[i]->rollNumber << endl;
-        cout << "SGPA :" << s[i]->SGPA << endl;
+        cout << "Student Name: " << s[i]->name << endl;
+        cout << "Roll Number: " << s[i]->rollNumber << endl;
+        cout << "SGPA: " << s[i]->SGPA << endl;
         cout << "---------------" << endl;
     }
 }
-void bubbleSort(struct Student *s[],int length)
+void bubbleSort(struct Student *s[], int length)
 {
-    for(int i=0;i<length-1;i++){
-        for(int j=i+1;j<length;j++){
-            if(s[j]->rollNumber<s[i]->rollNumber){
-                swap(s[i],s[j]);
+    for (int i = 0; i < length - 1; i++)
+    {
+        for (int j = i + 1; j < length; j++)
+        {
+            if (s[j]->rollNumber < s[i]->rollNumber)
+            {
+                swap(s[i], s[j]);
             }
-
         }
-
     }
 
-    DisplayStruct(s,length);
+    DisplayStruct(s, length);
 }
-void InsertionSort(struct Student *s[],int length)
+void InsertionSort(struct Student *s[], int length)
 {
-    for(int i=0;i<length;i++){
-        for(int j=i;j>0;j--){
-            if(s[j]->name<s[j-1]->name){
-                swap(s[j-1],s[j]);
-            }
-
+    for (int i = 1; i < length; i++)
+    {
+        Student *key = s[i];
+        int j = i - 1;
+        while (j >= 0 && s[j]->name > key->name)
+        {
+            s[j + 1] = s[j];
+            j = j - 1;
         }
-
+        s[j + 1] = key;
     }
 
-    DisplayStruct(s,length);
+    DisplayStruct(s, length);
 }
-void MergeSort(struct Student *s[],int length)
+int partition(struct Student *s[], int low, int high)
 {
+    float pivot = s[low]->SGPA;
+    int i = low + 1;
+    int j = high;
+
+    while (true)
+    {
+        while (i <= high && s[i]->SGPA <= pivot)
+            i++;
+        while (j >= low && s[j]->SGPA > pivot)
+            j--;
+        if (i < j)
+            swap(s[i], s[j]);
+        else
+            break;
+    }
+    swap(s[low], s[j]);
+    return j;
 }
-void operation(int input,Student *s[],int length)
+
+void QuickSort(struct Student *s[], int low, int high)
+{
+    if (low < high)
+    {
+        int pivot = partition(s, low, high);
+        QuickSort(s, low, pivot - 1);
+        QuickSort(s, pivot + 1, high);
+    }
+}
+
+void operation(int input, Student *s[], int length)
 {
     switch (input)
     {
     case 1:
-        bubbleSort(s,length);
+        bubbleSort(s, length);
         break;
     case 2:
-        InsertionSort(s,length);
+        InsertionSort(s, length);
         break;
     case 3:
-        MergeSort(s,length);
+        QuickSort(s, 0, length - 1);
+        DisplayStruct(s, length);
         break;
-
     default:
+        cout << "Invalid input. Please select a valid option." << endl;
         break;
     }
 }
 void DisplayUserOption(Student *s[], int lengthOfArr)
-{  
+{
     int input;
-    cout << "To sort the information according to rollNumber enter 1" << endl;
-    cout << "To sort the information according to Student name enter 2" << endl;
-    cout << "To sort the information according to SGPA 3" << endl;
+    cout << "Select Sorting Option:" << endl;
+    cout << "1. Sort by Roll Number" << endl;
+    cout << "2. Sort by Student Name" << endl;
+    cout << "3. Sort by SGPA" << endl;
     cin >> input;
-    operation(input,s,lengthOfArr);
+    operation(input, s, lengthOfArr);
 }
-
-// all code for sorting
-
-
 
 // Driver Code
 int main()
 {
     Student *s[20];
-    int count = 0, responce = 0;
+    int count = 0, response = 0;
     do
     {
 
-        cout << "Enter the name of Student :";
+        cout << "Enter the name of Student: ";
         string studentName;
         cin >> studentName;
-        cout << "Enter the roll number :";
+        cout << "Enter the roll number: ";
         int roll;
         cin >> roll;
-        cout << "Enter the SGPA of student :";
+        cout << "Enter the SGPA of student: ";
         float SGPA;
         cin >> SGPA;
         StudentInput(count, studentName, roll, SGPA, s);
         count++;
-        cout << "If want to countine the insertion of student record ,give input 1 to insert and 0 to exit ";
-        cin >> responce;
-    } while (responce != 0);
+        cout << "To continue inserting student records, enter 1. To exit, enter 0: ";
+        cin >> response;
+    } while (response != 0);
 
-    // just displaying the struct information
+    DisplayUserOption(s, count);
 
-    // DisplayStruct(s, count);
-    DisplayUserOption(s,count);
+    // Free allocated memory
+    for (int i = 0; i < count; i++)
+    {
+        delete s[i];
+    }
 
     return 0;
 }
